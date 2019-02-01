92eb2d6d3dc0ed7f6e822a8c195e53460c85ce04
fsorodrigues <desouzaoliveirarod.f@husky.neu.edu>
2019-02-01 18:58:09 -0500

    add simple blocking

--- matchit.py
+++ matchit.py
@@ -2,17 +2,39 @@
 import pandas as pd
 pd.options.display.float_format = '{:.2f}'.format
 from fuzzywuzzy import fuzz
+import re
 import time
+# define cleaning function
+def clean_it(string):
+    # define replace rules
+    replace_dict = {
+        '/':' ',
+        '-':' ',
+        '*':'',
+        '&':'',
+        ',':'',
+        '#':'',
+        '.':''
+    }
 
-# load data sets, perform filters and set data types
-prop_data = pd.read_csv('./properties.csv')
-prop_data['Owner Name 1'] = prop_data['Owner Name 1'].apply(lambda x: str(x))
+    # transform replace rules for regex understanting
+    rep = dict((re.escape(k), v) for k, v in replace_dict.items())
 
-corps = pd.read_csv('./corps.csv',low_memory=False)
-corps['BUSINESS_NAME'] = corps['BUSINESS_NAME'].apply(lambda x: str(x))
-corps = corps[(corps['BUSINESS_STATUS'] == 'Active') & (corps['BUSINESS_TYPE'] != 'Trade Name')]
+    # create regex patterns
+    pattern = re.compile('|'.join(rep.keys()))
+    the = re.compile(r'\bthe\b',re.I)
+    multiple_spaces = re.compile('\s{2,}')
 
-# create instance of function
+    # run replace
+    cleaned_text = pattern.sub(lambda x: replace_dict[x.group(0)],string)
+    cleaned_text = the.sub('',cleaned_text)
+    cleaned_text = re.sub(multiple_spaces,' ',cleaned_text)
+    cleaned_text = cleaned_text.strip()
+
+    #  return new string
+    return cleaned_text.lower()
+
+# create instance of function from fuzzywuzzy module
 ratio = fuzz.ratio
 set_ratio = fuzz.token_set_ratio
 
@@ -20,23 +42,38 @@ set_ratio = fuzz.token_set_ratio
 def match_it(series,dataframe):
     matches = []
     append_it = matches.append
-    
+
     for value in series:
-        for index,row in dataframe.iterrows():  
-            name = row['BUSINESS_NAME']
-            
-            if (ratio(value,name) < 93) or (set_ratio(value,name) < 97):
+        # create block
+        startswith = value[:7]
+        df = dataframe[dataframe['BUSINESS_NAME'].str.contains(startswith,case=False,na=False)]
+
+        for index,row in df.iterrows():
+            name = row['BUSINESS_NAME_clean']
+
+            # simple matching
+            if (name == value):
+                append_it(row['BUSINESS_ID'])
+
+            # fuzzy matching
+            elif (ratio(value,name) < 93) or (set_ratio(value,name) < 97):
                 append_it('NA')
             else:
                 append_it(row['BUSINESS_ID'])
-            
+
     return pd.Series(matches)
 
-# call function calculating the time it takes to execute
 start = time.time()
+# load data sets
+prop_data = pd.read_csv('./properties.csv')
+corps = pd.read_csv('./corps.csv',low_memory=False)
 
-dataset = prop_data.head(11).assign(BUSINESS_ID=lambda x: match_it(x['Owner Name 1'],corps))
+# run filters, set data types, and perform basic cleaning on strings
+prop_data['Owner Name 1_clean'] = prop_data['Owner Name 1'].apply(lambda x: clean_it(str(x)))
+# corps = corps[(corps['BUSINESS_STATUS'] == 'Active') & (corps['BUSINESS_TYPE'] != 'Trade Name')]
+corps['BUSINESS_NAME_clean'] = corps['BUSINESS_NAME'].apply(lambda x: clean_it(str(x)))
 
-end = time.time()
+# call function
 
-print(end - start)
+dataset = prop_data.head(11).assign(BUSINESS_ID=lambda x: match_it(x['Owner Name 1_clean'],corps))
+print(time.time() - start)
